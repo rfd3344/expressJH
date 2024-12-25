eca5e7d910724e748c2d7aa6fc6685097039fbe9
rfd3344w <rfd3344w@gmail.com>
2024-12-25 23:07:28 +1100

    Delete index copy.js

--- api/index copy.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel 3333"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
